a9d0aa171f38d5036882d07c474b254067dad699
Pholey <cassidy@launchbadge.com>
2015-04-14 21:43:34 -0700

    :art:

--- Placepuppy/Main.hs
+++ Placepuppy/Main.hs
@@ -5,14 +5,22 @@ import qualified Data.Text.Lazy as T
 import Data.Text.Lazy.Encoding (decodeUtf8)
 import qualified Views.Index
 import Text.Blaze.Html.Renderer.Text
+import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Static
 
 blaze = html . renderHtml
 
 main = scotty 9001 $ do
+  -- Log requests
+  middleware logStdoutDev
+
+  -- Set up static folder
+  middleware $ staticPolicy (noDots >-> addBase "static")
+
   get "/" $ do
     blaze Views.Index.render
 
-  matchAny "/:width/:height" $ do
+  matchAny "api/:width/:height" $ do
     width <- param "width"
     height <- param "height"
     html $ mconcat ["Width: ", width, " Height: ",  height]
--- Placepuppy/Views/Index.hs
+++ Placepuppy/Views/Index.hs
@@ -2,18 +2,16 @@
 
 module Views.Index (render) where
 
-import Web.Scotty
 import Text.Blaze.Html5
-import Text.Blaze.Html5.Attributes
-import qualified Web.Scotty as S
 import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html5.Attributes
 import Text.Blaze.Html.Renderer.Text
 
 render = do
-  H.html $ do
-    H.body $ do
+  html $ do
+    body $ do
       h1 "wow, such puppy"
       ul $ do
         li "much wag"
         li "woof"
+      img ! src "puppy.jpg" ! alt "puppy!"
